90cffb89bb75b5a966717fe9a13ebd68c7706c6a
Hugo Herrera <jaredh180@gmail.com>
2014-08-05 07:29:15 -0500

    Vibrar
    Es una aplicacion que son manda un mensaje.

---
+++ js/acciones.js
@@ -0,0 +1,14 @@
+//JavaScript
+	
+$(document).ready(function(e){
+	
+ 
+    $('#Beep').click(
+function(e){
+	alert('Hiciste click en el boton beep');
+  });//click Beep
+   $('#Vibrar').click(
+function(e){
+	alert('Hiciste click en el boton vibrar');
+  });//click Vibrar
+});//ready
