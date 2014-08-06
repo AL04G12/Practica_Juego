2ea775b77f4f56b24ccd007a5cb18c411b712934
Hugo Herrera <jaredh180@gmail.com>
2014-08-06 08:05:12 -0500

    Modificacion del tap
    Agregando tap

--- js/acciones.js
+++ js/acciones.js
@@ -1,12 +1,11 @@
 //JavaScript
 	var audio;
-$(document).ready(function(e){
-	if (window.plugins&&window.plugins.LowLatencyAudio)
 	
-	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX('mario','audio/mario.mp3', function(msg){},
-	    function(msg){alert('error: ' +msg);});
-
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		audio=window.plugins.LowLatencyAudio;
+		audio.preloadFX('mario','audio/mario.mp3',function(msg){},function(msg){alert('error: ' +msg);});
+	
     $('#Beep').bind ("tap",
 function(e){
 	navigator.notification.beep(1);
@@ -19,5 +18,5 @@ function(e){
 function(e){
 	audio.play('mario');
   });//click Vibrar
-  
+	});//device ready
 });//ready
